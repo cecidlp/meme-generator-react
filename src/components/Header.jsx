5a0731359da2bca4ccd31902864b11de5ed1c482
Cecilia de la Parra <delaparracecilia@gmail.com>
2022-11-18 14:32:29 +0100

    logo cat

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react"
+import logo from "/public/images/logo.png"
 
 export default function Header() {
     return (
         <div>
             <header className="header">
                 <div className="title-logo"> 
-                    <img src="/public/images/logo.png" alt="cat face logo" className="header-image"/>
+                    <img src={logo} alt="cat face logo" className="header-image"/>
                     <h1>Meme Generator</h1>
                 </div>
                     <h2 className="header-subtitle">React course - Project 3</h2>
