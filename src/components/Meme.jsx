a4b2a8df79c066405a8289edb319113b555a25b1
Cecilia de la Parra <delaparracecilia@gmail.com>
2022-11-18 14:09:24 +0100

    fetch from API

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import memesData from "/memesData.js";
+import React, { useState, useEffect } from "react";
 
 export default function Meme() {
     const [meme, setMeme] = useState({
@@ -8,12 +7,16 @@ export default function Meme() {
         randomImage: "http://i.imgflip.com/1bij.jpg" 
     })
 
-    const [allMemeImages, setAllMemeImages] = useState(memesData)
+    const [allMemes, setAllMemes] = useState([])
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
 
     function getMemeImage() {
-        const memesArray = allMemeImages.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
         setMeme(prevMeme => ({
             ...prevMeme,
             randomImage: url
@@ -56,7 +59,7 @@ export default function Meme() {
             onClick={getMemeImage}>
             Get a new meme image 🖼️
             </button>
-            
+
             <div className="meme">
                 <img src={meme.randomImage} className="meme--image" />
                 <h2 className="meme--text top">{meme.topText}</h2>
